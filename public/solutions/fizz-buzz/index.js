e2319e903f4df1418e70b0d1e5087f9244b75009
Ryan Morris <mr.morris@gmail.com>
2018-06-09 10:28:24 -0400

    Solution set

---
+++ public/solutions/fizz-buzz/index.js
@@ -0,0 +1,38 @@
+/**
+ * Exercise:
+ *
+ * FizzBuzz Function
+ *
+ * Work in the console, or use "console.log()" to output to the console 
+ * from the script
+ *
+ * The FizzBuzz rules...
+ *  - for numbers that are a multiple of 3, 
+ *    log "Fizz" instead of the number
+ *  - for numbers that are a multiple of 5
+ *    log "Buzz" instead of the number
+ *  - for numbers that are a multiple of both, 
+ *    log "FizzBuzz" instead of just Fizz or Buzz
+ *
+ * 1) Implement the fizzbuzz function:
+ *  - It accepts a single number argument
+ *  - Outputs the correct response to the console based on the FizzBuzz rules
+ *
+ * 2) Create a loop that executes your fizzbuzz function for the numbers from 1 to 100
+ *
+ * 3) BONUS 1 - write tests 
+ * use console.assert() to verify your function works as expected
+ * ex: console.assert(fizzbuzz(3) === "Fizz"); 
+ *
+ * 4) BONUS 2 - refactor your function
+ *  
+ */
+
+function fizzbuzz(n) {
+    // 1) implement the function
+}
+
+// 2) run the function with a loop
+
+// 3) BONUS 1
+// use console.assert() to test your function
---
+++ public/solutions/object-you/index.js
@@ -0,0 +1,34 @@
+/**
+ * Part 1: Create an object that represents yourself
+ *
+ * It should have some data (properties):
+ *  Give it your name
+ *  Height? Age? Whatever...
+ *
+ * It should have some behaviors (methods)
+ *  Speak? Sit? Stand?
+ *  Dance?
+ *
+ * Part 2: Create at least one method, "speak", which accepts a string argument and logs it to the console such as:
+ *   me.speak("hello") => "Tim says: hello"
+ *
+ * Part 3: Let’s assume you have a collection of something. Trophies?
+ * Create an array property on your object that has a couple trophies inside it
+ * 
+ * Part 4: Add a function that outputs the trophy name to the consold
+ *   me.viewTrophy(i)
+ * This should log the name of the trophy at index i to the console
+ *
+ * Part 5: Add a function that lists trophies to the console
+ *   me.listTrophies(i); 
+ * This should log a list of trophic names, separated by comma
+ * ex: “gold star, track, silver medal”
+ *
+ * Bonus A: Make the "trophies" property non-enumerable
+ * Hint: you'll need to use Object.defineProperty()
+ * Hint: for (var prop in me) {console.log(prop);} 
+ *       should not output "trophies" as an enumerable prop
+ *
+ * Bonus B: How might we make it so that your trophies are protected?
+ * Hint: closures
+*/
